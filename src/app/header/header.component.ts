2e6b185c97507404a1707d940fd5d43ea330f150
Allain Woodsford <awoodsford@hotmail.com>
2023-03-12 01:13:06 +1100

    working admin header link

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,23 +10,37 @@ import { Subscription } from 'rxjs';
 export class HeaderComponent implements OnInit, OnDestroy {
 
   private authenticationSub: Subscription;
+  private adminSub: Subscription;
   userAuthenticated = false;
+  adminStatus = '';
   constructor(private authService: AuthService){ }
 
   ngOnInit(): void{
-    //Not just relying on subscription to tell us if we are logged in or not, because the header might not have loaded before 
+    //Not just relying on subscription to tell us if we are logged in or not, because the header might not have loaded before
     //The app pushed out the subscription
     this.userAuthenticated = this.authService.getIsAuthenticated();
+    this.adminStatus = this.authService.getIsAdmin();
     this.authenticationSub = this.authService.getAuthenticatedSub().subscribe(status => {
       this.userAuthenticated = status;
     })
+    this.adminSub = this.authService.getAdminSub().subscribe(status => {
+      this.adminStatus = status;
+    });
   }
 
   ngOnDestroy(): void{
     this.authenticationSub.unsubscribe();
+    this.adminSub.unsubscribe();
   }
 
   logout(){
     this.authService.logout();
+  }
+
+  getIsAdmin(){
+    if(this.adminStatus === '1'){
+      return true;
+    }
+    return false;
   }
 }
